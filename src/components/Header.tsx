828b3def0d7764835f886663bb16322e878ef947
Michał <sukowskimichal@gmail.com>
2021-12-22 17:35:50 +0100

    Header cmp in build

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+    width: 100%;
+    padding: 20px 15px;
+    display: flex;
+    justify-content: center;
+    border: 1px solid black;
+`;
+
+const Header = () => {
+    return (
+        <StyledHeader>
+            <h1>E-Commerce Shop</h1>
+        </StyledHeader>
+    );
+};
+
+export default Header;

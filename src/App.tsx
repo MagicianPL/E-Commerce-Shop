0858d8d8f8f8faa9ae956b248e1700d868e915e3
Michał <sukowskimichal@gmail.com>
2021-12-24 09:51:59 +0100

    React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme/theme';
 import Header from './components/Header';
 import StyledHeroBg from './components/StyledHeroBg';
 import ProductsContainer from './components/ProductsContainer';
+import ProductDetail from './components/ProductDetail';
 
 function App() {
   return (
@@ -12,7 +18,12 @@ function App() {
       <ThemeProvider theme={theme}>
         <Header />
         <StyledHeroBg />
-        <ProductsContainer />
+      <Router>
+        <Routes>
+          <Route path="/" element={<ProductsContainer />} />
+          <Route path="/:id" element={<ProductDetail />} />
+        </Routes>
+      </Router>
       </ThemeProvider>
     </div>
   );

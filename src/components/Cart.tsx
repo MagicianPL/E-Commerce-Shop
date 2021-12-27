e979f261f19260ebfcff6419c29be72a804a3419
Michał <sukowskimichal@gmail.com>
2021-12-27 12:38:38 +0100

    cart cmp in build

---
+++ src/components/Cart.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Cart = () => {
+    return (
+        <>
+        <h1>CART Component</h1>
+        </>
+    );
+};
+
+export default Cart;

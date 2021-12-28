32f45fdeefdb5ba4316c9b2f7d76eedeb860ea66
Michał <sukowskimichal@gmail.com>
2021-12-28 11:45:11 +0100

    redux is working

--- src/components/ProductsContainer.tsx
+++ src/components/ProductsContainer.tsx
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { listProducts } from '../state/actions/productActions';
 import Product from './Product';
 
 const StyledWrapper = styled.div`
@@ -12,50 +14,21 @@ const StyledWrapper = styled.div`
 
 const ProductsContainer = () => {
 
-    interface IState {
-        products: {
-            _id: string
-            name: string
-            category: string
-            image: string
-            price: number
-            countInStock: number
-            brand: string
-            rating: number
-            numReviews: number
-            description: string
-        }[]
-    }
 
-    const [loadingData, setLoadingData] = useState(false);
-    const [products, setProducts] = useState<IState["products"]>([]);
-    const [error, setError] = useState<any>(false);
+    const productList = useSelector((state: any) => state.productList);
+    const {loading, error, products} = productList;
+    const dispatch = useDispatch();
 
     useEffect(()=>{
-        const fetchData = async() => {
-            await setLoadingData(true);
-
-            try {
-                const res = await fetch("http://localhost:5000/api/products");
-                const data = await res.json();
-                setProducts(data);
-                setLoadingData(false);
-            } catch(err) {
-                setError("Sorry, something gone wrong");
-                console.log(err);
-                setLoadingData(false);
-            };
-        };
-
-        fetchData();
-    }, []);
+        dispatch(listProducts())
+    }, [dispatch]);
 
     return (
         <StyledWrapper>
-            {loadingData ? <h1>Loading Data...</h1>
+            {loading ? <h1>Loading Data...</h1>
             : error ? <p>{error}</p>
             :
-            products.map(product => <Product key={product._id} product={product} />)}
+            products.map((product: any) => <Product key={product._id} product={product} />)}
         </StyledWrapper>
     );
 };

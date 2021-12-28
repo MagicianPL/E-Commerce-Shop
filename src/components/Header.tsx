5410b91f95fc9cd71ed0e80953fa370473f91e8d
Michał <sukowskimichal@gmail.com>
2021-12-28 16:06:46 +0100

    cart badge

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
 const StyledHeader = styled.header`
     width: 100%;
     padding: 30px 15px;
     display: flex;
+    flex-wrap: wrap;
     justify-content: space-around;
     color: ${({theme}) => theme.colors.primary};
 
@@ -18,14 +20,37 @@ const StyledHeader = styled.header`
             color: ${({theme}) => theme.colors.tertiary};
         }
     }
+
+    .cart {
+        display: flex;
+        align-items: center;
+
+        span {
+            width: 30px;
+            height: 30px;
+            border-radius: 50%;
+            background: ${({theme}) => theme.colors.primary};
+            color: ${({theme}) => theme.colors.secondary};
+            margin-left: 6px;
+            display: flex;
+            justify-content: center;
+            line-height: 22px;
+        }
+    }
 `;
 
 const Header = () => {
+    //cart array from redux store
+    const cartArr = useSelector((state: any) => state.cart.cart);
+    
     return (
         <StyledHeader>
             <p>Sign In</p>
             <h1>E-Commerce Shop</h1>
-            <p>Cart</p>
+            <p className="cart">
+                Cart
+                {cartArr.length > 0 ? <span>{cartArr.length}</span> : null}
+            </p>
         </StyledHeader>
     );
 };

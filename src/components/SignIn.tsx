566d1b47981739a4f4d6c5823aedee14f0d1886a
Michał <sukowskimichal@gmail.com>
2021-12-31 09:10:36 +0100

    SignIn cmp in build

---
+++ src/components/SignIn.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+    width: 100%;
+    max-width: 1000px;
+    margin: 0 auto;
+`;
+
+const SignIn = () => {
+    return (
+        <StyledWrapper>
+            
+        </StyledWrapper>
+    );
+};
+
+export default SignIn;

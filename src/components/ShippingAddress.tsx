77841daad020ad01a7bbf7664f5129348ff96f12
Michał <sukowskimichal@gmail.com>
2022-01-06 12:19:08 +0100

    ShippingAddress cmp in build

---
+++ src/components/ShippingAddress.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+    width: 100%;
+`;
+
+const ShippingAddress = () => {
+    return (
+        <StyledWrapper>
+            <h1>Shipping</h1>
+        </StyledWrapper>
+    );
+};
+
+export default ShippingAddress;
